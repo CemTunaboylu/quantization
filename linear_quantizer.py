cdbc4dedd0ae2ad85201a4e824a4b23f30e62f66
CompositeMan <cemtunaboylu9@gmail.com>
2024-06-05 18:11:16 +0300

    implement Quantization behaviors + per group

--- linear_quantizer.py
+++ linear_quantizer.py
@@ -15,7 +15,7 @@ class Mode(Enum):
 class Granularity(Enum):
     PerTensor = 0
     PerChannel = 1
-    # PerGroup = 2
+    PerGroup = 2
 
 
 """
@@ -26,6 +26,28 @@ class Granularity(Enum):
     s = (r_max - r_min) / (q_max - q_min)
     z = r/s - q
 """
+
+FloatOrTensor = Union[float, Tensor]
+IntOrTensor = Union[int, Tensor]
+
+
+class QuantizationParameters:
+
+    def __init__(
+        self,
+        scale: FloatOrTensor,
+        zero_point: IntOrTensor,
+        mode: Mode,
+        granularity: Granularity,
+        group_size: Union[int, None] = None,
+        dim: Union[int, None] = None,  # dim : [0, len(tensor.shape)]
+    ):
+        self.scale = scale
+        self.zero_point = zero_point
+        self.mode = mode
+        self.granularity = granularity
+        self.group_size = group_size
+        self.dim = dim
 
 
 # dim -> per rows : 0, per columns : 1
@@ -49,6 +71,24 @@ def per_channel_scale_and_zero_for(
     return scales, zeroes
 
 
+# returns the grouped tensor and the ungroup function
+def group(tensor: Tensor, by: int) -> Tuple[Tensor, Callable]:
+    shape = tensor.shape
+    if len(shape) != 2:
+        raise Exception(
+            f"tensors with more than 2 dimensions are not supported {shape}"
+        )
+
+    if (shape[0] * shape[1]) % by != 0:
+        raise Exception(f"tensor {shape} is not groupabled by {by}")
+    ungroup = lambda t: t.view(shape)
+
+    return tensor.view(-1, by), ungroup
+
+
+# finds and returns s and z for each group in the tensor. The compression rate depends on the group size and the mode
+# in symmetric mode, for float8 compression with float16 scale tensors and group size 32,
+# the compression rate has additional memory load of 16/32 = 0.5 coming from scales for each group.
 def get_info(data_type):
     return (
         iinfo(data_type) if isinstance(data_type, t_int.__class__) else finfo(data_type)
@@ -87,10 +127,8 @@ def clamp(val, _min, _max):
 quantization_strategies: List[Callable] = [
     get_scale_and_zero_for,
     per_channel_scale_and_zero_for,
+    per_channel_scale_and_zero_for,
 ]
-
-FloatOrTensor = Union[float, Tensor]
-IntOrTensor = Union[int, Tensor]
 
 
 def quantize_linear(
@@ -99,29 +137,55 @@ def quantize_linear(
     granularity: Granularity,
     mode: Mode,
     **additional_parameters,  # dim : [0, len(tensor.shape)]
-) -> Tuple[Tensor, FloatOrTensor, IntOrTensor]:
+) -> Tuple[Tensor, QuantizationParameters]:
     get_scale_and_zero: Callable[..., Tuple[FloatOrTensor, IntOrTensor]] = (
         quantization_strategies[granularity.value]
     )
 
-    if Granularity.PerChannel == granularity and "dim" in additional_parameters:
+    ungroup = None
+    dim, group_size = None, None
+
+    if Granularity.PerTensor == granularity:
+        (s, z) = get_scale_and_zero(tensor, data_type, mode)
+    else:
+        if not "dim" in additional_parameters:
+            raise Exception(
+                "dimension is required for per channel or per group quantizations"
+            )
+
+        if (
+            Granularity.PerGroup == granularity
+            and "group_size" in additional_parameters
+        ):
+            group_size = additional_parameters["group_size"]
+            (tensor, ungroup) = group(tensor, group_size)
+
         dim = additional_parameters["dim"]
         (s, z) = get_scale_and_zero(tensor, data_type, mode, dim)
+
         scale_shape = [1] * tensor.dim()
         scale_shape[dim] = -1
         s = s.view(scale_shape)
-    else:
-        (s, z) = get_scale_and_zero(tensor, data_type, mode)
+
+        z = z.view(scale_shape)
+
+    q_params = QuantizationParameters(s, z, mode, granularity, group_size, dim)
+
     # r/s - z = q
     rounded_tensor = t_round(tensor / s - z)
 
     d_info = get_info(data_type)
     d_min, d_max = d_info.min, d_info.max
 
-    return (rounded_tensor.clamp(d_min, d_max).to(data_type), s, z)
+    rounded_tensor = rounded_tensor.clamp(d_min, d_max).to(data_type)
+    if ungroup:
+        rounded_tensor = ungroup(rounded_tensor)
+
+    return (rounded_tensor, q_params)
 
 
-def dequantize_linear(
-    tensor: Tensor, scale: FloatOrTensor, zero: IntOrTensor
-) -> Tensor:
-    return scale * (tensor.float() - zero)
+def dequantize_linear(tensor: Tensor, parameters: QuantizationParameters) -> Tensor:
+    if Granularity.PerGroup == parameters.granularity:
+        (grouped, ungroup) = group(tensor.float(), parameters.group_size)
+        return ungroup(parameters.scale * (grouped - parameters.zero_point))
+    return parameters.scale * (tensor.float() - parameters.zero_point)
